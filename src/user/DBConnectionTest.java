0fe2df94890b363405ffb7fdb1809d1f51a2461f
won-gg <xxnwon@ewha.ac.kr>
2026-05-19 00:37:44 +0900

    feat: DBConnectionTest

---
+++ src/user/DBConnectionTest.java
@@ -0,0 +1,21 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DBConnectionTest {
+
+    public static void main(String[] args) {
+        try {
+            Connection conn = DBConnector.getConnection();
+
+            System.out.println("DB 연결 성공!");
+            System.out.println("현재 DB: " + conn.getCatalog());
+
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("DB 연결 실패");
+            e.printStackTrace();
+        }
+    }
+}
--- src/user/DBConnector.java
+++ src/user/DBConnector.java
@@ -6,10 +6,10 @@ import java.sql.SQLException;
 
 public class DBConnector {
 	
-		//ip 확인후 수정합니다.
-		private static final String URL = "jdbc:mysql://개인 url";  
-		private static final String USER = "유저명";
-	 	private static final String PASS = "패스워드"; //자프실2
+		//Ip 확인후 수정합니다.
+		private static final String URL = "jdbc:mysql://개인로컬DB/seathub";  
+		private static final String USER = "username";
+	 	private static final String PASS = "password";
 
 		
 		public static Connection getConnection() throws SQLException {
