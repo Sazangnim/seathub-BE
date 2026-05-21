ca51fdcc968379f9bb12d0755a5244e59cb2d373
won-gg <xxnwon@ewha.ac.kr>
2026-05-21 18:58:36 +0900

    fix: 기존 패키지 수정 및 생성

--- src/DBConnect/DBConnectionTest.java
+++ src/DBConnect/DBConnectionTest.java
@@ -1,4 +1,4 @@
-package user;
+package DBConnect;
 
 import java.sql.Connection;
 import java.sql.SQLException;
--- src/DBConnect/DBConnector.java
+++ src/DBConnect/DBConnector.java
@@ -1,4 +1,4 @@
-package user;
+package DBConnect;
 
 import java.io.FileInputStream;
 import java.io.IOException;
