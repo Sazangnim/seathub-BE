315a1a80434eafc16fba79bfc1302974a0566716
won-gg <xxnwon@ewha.ac.kr>
2026-06-01 15:54:51 +0900

    fix: business_number 표현 방식 수정

--- db/11_update_dummy_data.sql
+++ db/11_update_dummy_data.sql
@@ -8,3 +8,8 @@ UPDATE study_cafe SET region = 'Jongno'  WHERE cafe_id = 4;
 
 -- 태그 Premium → Laptop 수정
 UPDATE tag SET tag_name = 'Laptop' WHERE tag_name = 'Premium';
+
+-- 사업자등록번호 수정
+UPDATE user SET business_number = '1234567890' WHERE user_id = 1;
+UPDATE user SET business_number = '2345678901' WHERE user_id = 12;
+UPDATE user SET business_number = '3456789012' WHERE user_id = 13;
